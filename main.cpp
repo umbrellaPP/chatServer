aef2df499e657e31e91181d35de28d616f1df054
PP <chinese1236@qq.com>
2018-03-25 08:41:49 +0800

    db init

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include <QCoreApplication>
+﻿#include <QCoreApplication>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QDebug>
+#include "db/userdb.h"
+
+#include <QSqlQuery>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QSqlDatabase::addDatabase("QMYSQL");
 
+    UserDB *userDB = new UserDB();
+//    userDB->addUser("Test", "wqqqqqq");
+//    userDB->removeUser("14533");
+//    userDB->selectAllUsers();
+//    userDB->selectUser("14569");
+//    userDB->updateUser("14568", "PP", "wqnmlgsb");
     return a.exec();
 }
